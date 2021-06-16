df0b7538c0dce686c90101d8b56429199af82b24
Jon McDonald <Jon.McDonald@ibm.com>
2021-06-16 13:01:11 -0500

    added schema helper files

---
+++ deployments/schema/deployments.sql
@@ -0,0 +1,2 @@
+Create table deploymentslist(id INTEGER PRIMARY KEY, deploymentdate DATE, deploymenttime TIME);
+Create sequence contacts_seq start with 1 increment by 1;
---
+++ recovery/schema/recoveryschema.sql
@@ -0,0 +1,2 @@
+Create table recovertimeslist(id INTEGER PRIMARY KEY, recstartdate DATE, recstarttime TIME, recduration INTEGER);
+Create sequence rectimes_seq start with 1 increment by 1;
