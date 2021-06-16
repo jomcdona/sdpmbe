a4d70a9da07d349f8b6aa46f3b55e5b0045fb956
Jon McDonald <Jon.McDonald@ibm.com>
2021-06-16 12:51:46 -0500

    deployments refactoring

---
+++ deployments/src/test/java/com/ibm/garage/sdpmbe/DeploymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.garage.sdpmbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
